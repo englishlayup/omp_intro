4adb77060248930492c03c70528fdc05ec3919d7
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-11-14 18:36:03 -0500

    excerise 3 in 07 Module 4
    - Not sure if it's what he wants since the solution is performant with
    or without the synchronization construct

--- src/lib.c
+++ src/lib.c
@@ -2,35 +2,36 @@
 #include <omp.h>
 
 #define THREAD_COUNT 12
-#define PAD 8
 
 static long num_steps = 1e9;
 double step;
 
 double parallel_pi() {
   double pi = 0.0, num_threads;
-  double sum[THREAD_COUNT][PAD];
+  double sum[THREAD_COUNT] = {};
 
   step = 1.0 / (double)num_steps;
   omp_set_num_threads(THREAD_COUNT);
 #pragma omp parallel
   {
     int i, id, nthrds;
-    double x;
+    double x, thread_sum;
     nthrds = omp_get_num_threads();
     id = omp_get_thread_num();
 
     if (id == 0)
       num_threads = nthrds;
 
-    for (i = id, sum[id][0] = 0.0; i < num_steps; i = i + nthrds) {
+    for (i = id, thread_sum = 0.0; i < num_steps; i = i + nthrds) {
       x = (i + 0.5) * step;
-      sum[id][0] += 4.0 / (1.0 + x * x);
+      thread_sum += 4.0 / (1.0 + x * x);
     }
+#pragma omp atomic
+    sum[id] += thread_sum;
   }
 
   for (int i = 0; i < num_threads; i++) {
-    pi += sum[i][0] * step;
+    pi += sum[i] * step;
   }
 
   return pi;
