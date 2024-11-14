33cfa1fba02cf3ff830c1aa056738540b6ef871a
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-11-14 14:11:41 -0500

    solution from the course

--- src/main.c
+++ src/main.c
@@ -1,37 +1,41 @@
 #include <omp.h>
 #include <stdio.h>
 
+#define THREAD_COUNT 2
+
 static long num_steps = 100000;
 double step;
-static int THREAD_COUNT = 10;
 
-double parallel_integral() {
-  double x, pi, sum = 0.0;
-  double sums[THREAD_COUNT] = {};
+double parallel_pi() {
+  double pi, num_threads;
+  double sum[THREAD_COUNT] = {};
 
   step = 1.0 / (double)num_steps;
   omp_set_num_threads(THREAD_COUNT);
 #pragma omp parallel
   {
-    int num_threads = omp_get_num_threads();
-    int thread_id = omp_get_thread_num();
-    int start = thread_id * (num_steps / num_threads);
-    int end = num_steps / num_threads + start - 1;
-    for (int i = start; i < end; i++) {
+    int i, id, nthrds;
+    double x;
+    nthrds = omp_get_num_threads();
+    id = omp_get_thread_num();
+
+    if (id == 0)
+      num_threads = nthrds;
+
+    for (i = id, sum[id] = 0.0; i < num_steps; i = i + nthrds) {
       x = (i + 0.5) * step;
-      sums[thread_id] += 4.0 / (1.0 + x * x);
+      sum[id] += 4.0 / (1.0 + x * x);
     }
   }
 
-  for (int i = 0; i < THREAD_COUNT; i++) {
-    sum += sums[i];
+  for (int i = 0; i < num_threads; i++) {
+    pi += sum[i] * step;
   }
-  pi = step * sum;
 
   return pi;
 }
 
-double sequential_integral() {
+double sequential_pi() {
   int i;
   double x, pi, sum = 0.0;
   step = 1.0 / (double)num_steps;
@@ -46,13 +50,13 @@ double sequential_integral() {
 int main() {
   double pi, start, end;
   start = omp_get_wtime();
-  pi = sequential_integral();
+  pi = sequential_pi();
   end = omp_get_wtime();
   printf("Sequential pi = %f\n", pi);
   printf("Took %f\n", (end - start) * 1e6);
 
   start = omp_get_wtime();
-  pi = parallel_integral();
+  pi = parallel_pi();
   end = omp_get_wtime();
   printf("Parallel pi = %f\n", pi);
   printf("Took %f\n", (end - start) * 1e6);
