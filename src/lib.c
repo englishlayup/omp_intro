61ce6f2fedcb73e9d8f9960db5d347eb553c7727
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-11-16 02:38:10 -0500

    exercise 4 in 09 part 2 Module 5

--- src/lib.c
+++ src/lib.c
@@ -7,28 +7,15 @@ static long num_steps = 1e9;
 double step;
 
 double parallel_pi() {
-  double pi = 0.0, num_threads;
-
+  int i;
+  double pi, sum = 0.0;
   step = 1.0 / (double)num_steps;
-  omp_set_num_threads(THREAD_COUNT);
-#pragma omp parallel
-  {
-    int i, id, nthrds;
-    double x, sum;
-    nthrds = omp_get_num_threads();
-    id = omp_get_thread_num();
-
-    if (id == 0)
-      num_threads = nthrds;
-
-    for (i = id, sum = 0.0; i < num_steps; i = i + nthrds) {
-      x = (i + 0.5) * step;
-      sum += 4.0 / (1.0 + x * x);
-    }
-#pragma omp atomic
-    pi += sum * step;
+#pragma omp parallel for reduction(+ : sum)
+  for (i = 0; i < num_steps; i++) {
+    double x = (i + 0.5) * step;
+    sum = sum + 4.0 / (1.0 + x * x);
   }
-
+  pi = step * sum;
   return pi;
 }
 
--- src/main.c
+++ src/main.c
@@ -4,11 +4,6 @@
 
 int main() {
   double pi, start, end;
-  start = omp_get_wtime();
-  pi = sequential_pi();
-  end = omp_get_wtime();
-  printf("Sequential pi = %f\n", pi);
-  printf("Took %f seconds\n", (end - start));
 
   start = omp_get_wtime();
   pi = parallel_pi();
