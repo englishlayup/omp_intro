bae139a92403ccf215643b73d59799977f09c529
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-11-23 22:22:53 -0500

    my solution for ex6 in 16 Module 9

--- src/linked.c
+++ src/linked.c
@@ -68,24 +68,12 @@ int main(int argc, char *argv[]) {
   head = p;
 
   start = omp_get_wtime();
-#pragma omp parallel firstprivate(p)
+#pragma omp parallel
+#pragma omp single
   {
-    int id = omp_get_thread_num();
-    int nthreads = omp_get_num_threads();
-
-    for (int i = 0; i < id; i++) {
-      if (p == NULL)
-        break;
-      p = p->next;
-    }
-
-    while (p != NULL) {
+    for (p = head; p != NULL; p = p->next) {
+#pragma omp task firstprivate(p)
       processwork(p);
-      for (int i = 0; i < nthreads; i++) {
-        if (p == NULL)
-          break;
-        p = p->next;
-      }
     }
   }
 
